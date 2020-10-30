6a04e3a60db0965e640efc324e058cee738f30b0
neo <1733112322@qq.com>
2020-10-30 09:19:51 +0800

    core sql  connecting

---
+++ WindowsFormsCoreDemo4SQL/Form1.cs
@@ -0,0 +1,132 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsCoreDemo4SQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "127.0.0.1",
+                Database = "ccf02",
+                UserID = "root",
+                Password = "root",
+                //SslMode = MySqlSslMode.Required,
+            };
+
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                Console.WriteLine("Opening connection");
+                 conn.Open();
+
+                using (var command = conn.CreateCommand())
+                {
+                    command.CommandText = "DROP TABLE IF EXISTS inventory;";
+                     command.ExecuteNonQueryAsync();
+                    Console.WriteLine("Finished dropping table (if existed)");
+
+                    command.CommandText = "CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);";
+                     command.ExecuteNonQueryAsync();
+                    Console.WriteLine("Finished creating table");
+
+                    command.CommandText = @"INSERT INTO inventory (name, quantity) VALUES (@name1, @quantity1),
+                        (@name2, @quantity2), (@name3, @quantity3);";
+                    command.Parameters.AddWithValue("@name1", "banana");
+                    command.Parameters.AddWithValue("@quantity1", 150);
+                    command.Parameters.AddWithValue("@name2", "orange");
+                    command.Parameters.AddWithValue("@quantity2", 154);
+                    command.Parameters.AddWithValue("@name3", "apple");
+                    command.Parameters.AddWithValue("@quantity3", 100);
+
+                    //int rowCount =  command.ExecuteNonQueryAsync();
+                    ////Console.WriteLine(String.Format("Number of rows inserted={0}", rowCount));
+                }
+
+                // connection will be closed by the 'using' block
+                Console.WriteLine("Closing connection");
+            }
+
+            Console.WriteLine("Press RETURN to exit");
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string connString = "server=localhost;database=ccf02;uid=root;pwd=root";
+            MySqlConnection conn = new MySqlConnection(connString);
+            try
+            {
+                conn.Open();
+                MessageBox.Show("连接成功！", "测试结果");
+                MySqlDataReader reader = null;
+
+                MySqlCommand cmd = new MySqlCommand("SHOW DATABASES", conn);
+
+
+
+                try
+
+                {
+
+                    reader = cmd.ExecuteReader();
+
+                    //databaseList.Items.Clear();
+
+                    while (reader.Read())
+
+                    {
+
+                        //databaseList.Items.Add(reader.GetString(0));
+
+                    }
+
+                }
+
+                catch (MySqlException ex)
+
+                {
+
+                    MessageBox.Show("Failed to populate database list: " + ex.Message);
+
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+    }
+}
